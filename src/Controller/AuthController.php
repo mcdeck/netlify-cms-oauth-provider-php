69a0f9aab8d4aa6a5eb9653eb6c8a0f62f98681f
Oliver van Porten <oliver@van-porten.de>
2021-01-23 20:19:32 +0100

    Initial commit

---
+++ src/Controller/AuthController.php
@@ -0,0 +1,67 @@
+<?php
+
+namespace App\Controller;
+
+use Exception;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Routing\Annotation\Route;
+
+use App\Service\AuthService;
+
+class AuthController extends AbstractController
+{
+    /**
+     * @Route("/")
+     */
+    public function index(): Response
+    {
+        return $this->render('index.html.twig', []);
+    }
+
+    /**
+     * @Route("/auth")
+     */
+    public function auth(Request $request, AuthService $authService): Response
+    {
+        return $authService->processRequest($request);
+    }
+
+    /**
+     * @Route("/callback")
+     */
+    public function callback(Request $request, AuthService $authService): Response
+    {
+        $oauthProvider = $this->getParameter('app.oauthProvider');
+        $origin = $this->getParameter('app.origin');
+
+        try {
+            $content = [
+                'token' => $authService->getToken($request),
+                'provider' => $oauthProvider
+            ]; 
+            $message = 'success';
+        } catch(Exception $e) {
+            $content = $e;
+            $message = "error";
+        }   
+        $content = json_encode($content);
+
+        return $this->render('redirect.html.twig', [
+            'message' => "authorization:{$oauthProvider}:{$message}:{$content}",
+            'origin' => $origin,
+            'oauthProvider' => $oauthProvider
+        ]);
+    }
+
+    /**
+     * @Route("/success")
+     */
+    public function success(): Response
+    {
+        return new Response('', 204);
+    }
+}
+
+?>
---
+++ src/Service/AuthService.php
@@ -0,0 +1,82 @@
+<?php 
+
+namespace App\Service;
+
+use League\OAuth2\Client\Provider\Github;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\RedirectResponse;
+use Symfony\Component\HttpFoundation\Session\SessionInterface;
+use Symfony\Component\DependencyInjection\ParameterBag\ContainerBagInterface;
+use Exception;
+
+class AuthService
+{
+    private $provider;
+    private $session;
+    private $params;
+
+    public function __construct(SessionInterface $session, ContainerBagInterface $params)
+    {
+        $this->provider = new Github([
+            'clientId'          => $params->get('app.clientId'),
+            'clientSecret'      => $params->get('app.clientSecret'),
+            'redirectUri'       => $params->get('app.redirectUri'),
+            'domain'            => $params->get('app.domain')
+        ]);
+
+        $this->session = $session;
+
+        $this->params = $params;
+    }
+
+    private function isCodeSet(Request $request) : bool 
+    {
+        return $request->query->get('code', null) !== null;
+    }
+
+    private function isSessionInvalid(Request $request) : bool 
+    {
+        return empty($request->query->get('state', null)) || ($request->query->get('state') !== $this->session->get('oauth2state', null));
+    }
+
+    private function isSessionValid(Request $request) : bool 
+    {
+        return !$this->isSessionInvalid($request);
+    }
+
+    public function processRequest(Request $request): Response
+    {
+        if (!$this->isCodeSet($request)) {
+            // If we don't have an authorization code then get one
+            $options = [
+                'state' => bin2hex(random_bytes(64)),
+                'scope' => $this->params->get('app.scopes') // array or string
+            ];
+            
+            $authUrl = $this->provider->getAuthorizationUrl($options);
+            $this->session->set('oauth2state', $this->provider->getState());
+            return new RedirectResponse($authUrl);
+
+        // Check given state against previously stored one to mitigate CSRF attack
+        } elseif ($this->isSessionInvalid($request)) {
+            $this->session->remove('oauth2state');
+            return new Response($content = 'Invalid state', $status = 500);
+        }
+    }
+
+    public function getToken(Request $request): string
+    {
+        if ($this->isSessionValid($request)) {
+            // Try to get an access token (using the authorization code grant)
+            $token = $this->provider->getAccessToken('authorization_code', [
+                'code' => $request->query->get('code')
+            ]);
+
+            // Use this to interact with an API on the users behalf
+            return $token->getToken();
+        } else {
+            throw new Exception("Invalid Session!");
+        }
+    }
+}
